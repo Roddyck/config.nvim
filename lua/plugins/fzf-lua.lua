09bf2cbab44e66860f24f89f589068fcde63407c
Roddyck <rodion.ivashchenko2003@gmail.com>
2025-05-24 02:02:31 +0300

    trying out fzf-lua instead of telescope

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    config = function()
+      local fzf_lua = require("fzf-lua")
+      fzf_lua.setup({
+        "telescope",
+
+        fzf_colors = {
+          true,
+          bg = "-1",
+          gutter = "-1",
+        },
+      })
+
+      vim.keymap.set("n", "<leader>pf", fzf_lua.files)
+      vim.keymap.set("n", "<leader>ef", function()
+        fzf_lua.files({ cwd = "~/personal/dev/env/.config/nvim" })
+      end)
+      vim.keymap.set("n", "<C-p>", fzf_lua.git_files)
+      vim.keymap.set("n", "<leader>ps", fzf_lua.grep)
+      vim.keymap.set("n", "<leader>lg", fzf_lua.live_grep)
+      vim.keymap.set("n", "<leader>vh", fzf_lua.helptags)
+    end,
+  },
+}
