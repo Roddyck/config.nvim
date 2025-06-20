72c346f540f85e042d6c1c314963c87926d6677a
Roddyck <rodion.ivashchenko2003@gmail.com>
2025-06-21 00:48:08 +0300

    autocommit to dev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -119,9 +119,8 @@ return {
 
     vim.diagnostic.config({
       -- update_in_insert = true,
+      virtual_text = true,
       float = {
-        focusable = false,
-        style = "minimal",
         border = "rounded",
         source = true,
         header = "",
