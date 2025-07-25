d21f3444bea1c96dfe487896d2e55d0607276305
Roddyck <rodion.ivashchenko2003@gmail.com>
2025-07-25 22:17:49 +0300

    autocommit to dev

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -18,6 +18,9 @@ return {
       go = { "gofmt" },
       -- Conform will run the first available formatter
       javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
     },
 
     default_format_opts = {
