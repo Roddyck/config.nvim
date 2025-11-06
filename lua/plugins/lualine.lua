21c3d54d8ae3142bc5d85aa423f995fd11390a97
Roddyck <rodion.ivashchenko2003@gmail.com>
2025-11-06 18:35:53 +0300

    autocommit to dev

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,6 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
-    options = { theme = "gruvbox_dark" },
+    options = { theme = "auto" },
   },
 }
