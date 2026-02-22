d30af982f721336342def429aa2add03c1b04d03
Roddyck <rodion.ivashchenko2003@gmail.com>
2026-02-22 16:07:33 +0300

    autocommit to dev

--- lua/plugins/coqtail.lua
+++ lua/plugins/coqtail.lua
@@ -1,3 +1,11 @@
 return {
   "whonore/Coqtail",
+  ft = "coq",
+  config = function()
+    vim.g.coqtail_nomap = 1
+    vim.keymap.set("n", "<leader>cc", "<cmd>RocqStart<cr>")
+    vim.keymap.set("n", "<leader>cq", "<cmd>RocqStop<cr>")
+    vim.keymap.set("n", "<leader>cj", "<cmd>RocqNext<cr>")
+    vim.keymap.set("n", "<leader>ck", "<cmd>RocqUndo<cr>")
+  end,
 }
