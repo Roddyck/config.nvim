f96be14f47fc126232f748f54ee53ca8ea18e782
Roddyck <rodion.ivashchenko2003@gmail.com>
2026-04-18 17:51:32 +0300

    some moving around of autocmds

--- init.lua
+++ init.lua
@@ -5,18 +5,6 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 local yank_group = augroup("HighlightYank", {})
-local RdkGroup = augroup("Rdk", {})
-
-autocmd("FileType", {
-  callback = function(args)
-    local bufnr = args.buf
-    local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
-    if not ok or not parser then
-      return
-    end
-    pcall(vim.treesitter.start)
-  end,
-})
 
 autocmd("TextYankPost", {
   group = yank_group,
@@ -26,42 +14,5 @@ autocmd("TextYankPost", {
       higroup = "IncSearch",
       timeout = 40,
     })
-  end,
-})
-
-autocmd("LspAttach", {
-  group = RdkGroup,
-  callback = function(e)
-    local opts = { buffer = e.buf }
-    vim.keymap.set("n", "gd", function()
-      vim.lsp.buf.definition()
-    end, opts)
-    vim.keymap.set("n", "K", function()
-      vim.lsp.buf.hover()
-    end, opts)
-    vim.keymap.set("n", "<leader>vws", function()
-      vim.lsp.buf.workspace_symbol()
-    end, opts)
-    vim.keymap.set("n", "<leader>vd", function()
-      vim.diagnostic.open_float()
-    end, opts)
-    vim.keymap.set("n", "<leader>vca", function()
-      vim.lsp.buf.code_action()
-    end, opts)
-    vim.keymap.set("n", "<leader>vrr", function()
-      vim.lsp.buf.references()
-    end, opts)
-    vim.keymap.set("n", "<leader>vrn", function()
-      vim.lsp.buf.rename()
-    end, opts)
-    vim.keymap.set("i", "<C-h>", function()
-      vim.lsp.buf.signature_help()
-    end, opts)
-    vim.keymap.set("n", "[d", function()
-      vim.diagnostic.jump({ count = 1, float = true })
-    end, opts)
-    vim.keymap.set("n", "]d", function()
-      vim.diagnostic.jump({ count = -1, float = true })
-    end, opts)
   end,
 })
--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -25,23 +25,18 @@ local ensure_installed = {
   "rust",
   "bash",
 }
+
 require("nvim-treesitter").install(ensure_installed)
 
-vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter-context" },
-})
-require("treesitter-context").setup({
-  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-  multiwindow = false, -- Enable multiwindow support.
-  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-  line_numbers = true,
-  multiline_threshold = 20, -- Maximum number of lines to show for a single context
-  trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-  mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-  -- Separator between context and content. Should be a single character string, like '-'.
-  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-  separator = nil,
-  zindex = 20, -- The Z-index of the context window
-  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+local group = vim.api.nvim_create_augroup("RoddykTreesitter", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+  group = group,
+  callback = function(args)
+    local bufnr = args.buf
+    local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+    if not ok or not parser then
+      return
+    end
+    pcall(vim.treesitter.start, 0)
+  end,
 })
