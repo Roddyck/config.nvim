dc673c0157f84328890dca8eb2634fd73ba78955
Roddyck <rodion.ivashchenko2003@gmail.com>
2026-04-29 22:29:40 +0300

    going back to telescope for no reason. Actually, telescope is pretty cool.

--- plugin/neogit.lua
+++ plugin/neogit.lua
@@ -2,7 +2,11 @@ vim.pack.add({
   { src = "https://github.com/NeogitOrg/neogit" },
   { src = "https://github.com/nvim-lua/plenary.nvim" }, -- required
   { src = "https://github.com/sindrets/diffview.nvim" }, -- optional - Diff integration
-  { src = "https://github.com/sindrets/ibhagwan/fzf-lua" }, -- optional
+
+  {
+    src = "https://github.com/nvim-telescope/telescope.nvim",
+    version = vim.version.range("0.2.*"),
+  }, -- optional
 })
 
 vim.keymap.set("n", "<leader>gs", "<cmd>Neogit<cr>", { desc = "Neogit" })
